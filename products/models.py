4af4e539b88640b78df4d9e7fd7bcf04871a4752
MaireadKelly <mairead.kelly26@gmail.com>
2025-07-30 18:44:07 +0100

    Add Product and Category models, load fixtures data and custom admin formatting

---
+++ products/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='subcategories', on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name if self.friendly_name else self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    is_custom = models.BooleanField(default=False)
+    is_accessory = models.BooleanField(default=False)
+    is_offer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
