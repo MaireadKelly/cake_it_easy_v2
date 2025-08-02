6a3b5ad914fd6de56638ee2266480d7f242310a1
MaireadKelly <mairead.kelly26@gmail.com>
2025-08-02 22:15:13 +0100

    Add product views, templates and Cloudinary image support

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# products/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-# Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/product_list.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/product_detail.html', {'product': product})
