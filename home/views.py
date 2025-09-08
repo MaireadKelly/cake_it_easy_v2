518b3b3ab562c750475f306a1e7906e2ff18f054
MaireadKelly <mairead.kelly26@gmail.com>
2025-09-08 02:18:44 +0100

    fix:(home): explicit URL)

--- home/views.py
+++ home/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from products.models import Product
 
-""" A view to return to the home page """
-def home(request):
-    products = Product.objects.all()
-    return render(request, 'home/index.html', {'products': products})
+def index(request):
+    return render(request, 'home/index.html')
