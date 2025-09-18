e85d20aad3a7ee99ed3f08ad3dd57224791d36c2
MaireadKelly <mairead.kelly26@gmail.com>
2025-09-18 19:21:52 +0100

    whitenoise missing from Middleware!

--- cake_it_easy_v2/settings.py
+++ cake_it_easy_v2/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
