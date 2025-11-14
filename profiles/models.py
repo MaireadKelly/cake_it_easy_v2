1836a36774911f920adadac600dc5f25af2b5046
MaireadKelly <mairead.kelly26@gmail.com>
2025-11-14 09:26:00 +0000

    Added County to User Profiles

--- profiles/models.py
+++ profiles/models.py
@@ -15,9 +15,11 @@ class UserProfile(models.Model):
     default_phone_number = models.CharField(max_length=32, blank=True)
     default_street_address1 = models.CharField(max_length=80, blank=True)
     default_street_address2 = models.CharField(max_length=80, blank=True)
+    default_town_or_city = models.CharField(max_length=40, blank=True)
+    default_county = models.CharField(max_length=40, blank=True)
     default_country = models.CharField(max_length=40, blank=True)
     default_postcode = models.CharField(max_length=20, blank=True)
-    default_town_or_city = models.CharField(max_length=40, blank=True)
+    
 
     def __str__(self):
         return f"{self.user.username}"
